de402a04105f39e0dae89d9d0cfdbda9567b8671
Swaraj-N <SWARAJ-PC@SWARAJ-PC>
2022-09-22 22:07:13 +0530

    Commit pushed at 10:08

---
+++ src/test/java/org/PracticeTestScripts/Practice6Test.java
@@ -0,0 +1,13 @@
+package org.PracticeTestScripts;
+
+import org.testng.Reporter;
+import org.testng.annotations.Test;
+
+public class Practice6Test {
+
+	@Test
+	public void practiceScrip6()
+	{
+		Reporter.log("Script5 Added at 10:10 PM",true);
+	}
+}
